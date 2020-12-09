253d491d4215488b751c8add945229fd27ca47d8
GiovanniSalvi <salvigiovanni4@gmail.com>
2020-12-09 12:12:41 +0000

    background image added hidden text created in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//let image = document.getElementById('bulb_one_off');
+//let image = document.querySelector(".bulb-off")
     
     //image.addEventListener('click', function(){
         //changeImage()
@@ -7,7 +7,7 @@
 
     //function changeImage(){
         //image.src = "https://www.iconarchive.com/download/i45250/iconleak/or/light-bulb.ico"
-    //};
+//};
 let computerSerie = [];
 let userSerie = [];
 let level = 0;
@@ -15,13 +15,14 @@ let level = 0;
 const startButton = document.getElementById("startButton");
 const sequenceLevel = document.querySelector(".waitMode");  //
 const sectionContainer = document.querySelector(".container")
-const levelsCounter = document.querySelector("counter")
+const levelsCounter = document.querySelector(".counter")
 
 startButton.addEventListener('click', startGame);
 
 function startGame() {
     startButton.classList.add('hidden');
     sequenceLevel.classList.remove('hidden');
+    levelsCounter.classList.remove('counter');
     nextTurn();
 }
 
