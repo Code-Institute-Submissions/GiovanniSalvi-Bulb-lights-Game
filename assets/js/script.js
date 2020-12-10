b0c8827b40bc31583f2d106ae4914c56b1774578
GiovanniSalvi <salvigiovanni4@gmail.com>
2020-12-10 03:21:04 +0000

    script.js nearly done responsive layout to be completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,65 +6,126 @@
     //});
 
     //function changeImage(){
-        //image.src = "https://www.iconarchive.com/download/i45250/iconleak/or/light-bulb.ico"
+        //image.src = ""
 //};
 let computerSerie = [];
 let userSerie = [];
 let level = 0;
 
 const startButton = document.getElementById("startButton");
-const sequenceLevel = document.querySelector(".waitMode");  //
-const sectionContainer = document.querySelector(".container")
-const levelsCounter = document.querySelector(".counter")
-const endGame = document.querySelector(".success")
-const gameLost = document.querySelector(".lost")
+const sequenceLevel = document.querySelector(".waitMode");  
+const sectionContainer = document.querySelector(".container");
+const levelsCounter = document.querySelector(".counter");
+const endGame = document.querySelector(".success");
+const gameLost = document.querySelector(".lost");
 
 function startGame() {
-    startButton.classList.add('hidden');
-    sequenceLevel.classList.remove('hidden');
-    levelsCounter.classList.remove('counter');
-    nextTurn();
+      startButton.classList.add('hidden');
+      sequenceLevel.classList.remove('hidden');
+      levelsCounter.classList.remove('counter');
+      nextTurn();
 }
+
+startButton.addEventListener('click', startGame);
+sectionContainer.addEventListener('click', event => {
+  const { bulb } = event.target.dataset;
+
+  if (bulb) manageTap(bulb);
+});
+
 
 function nextTurn() {
    level += 1;
    sectionContainer.classList.add('unclickable');
    levelsCounter.textContent = `Level ${level} of 10`;
-
+   
+    const nextSerie = [...computerSerie];
+     nextSerie.push(chooseBulb());
+     
+     switchLight(nextSerie);
+     
+     computerSerie = [...nextSerie];
+     setTimeout(() => {
+     userTurn(level);
+    }, level * 500 + 1000);
 }
 
-startButton.addEventListener('click', startGame);
+function chooseBulb(){
+    const  bulbs = ['one', 'two', 'three', 'four', 'five', 'six' ];
+    const  random = bulbs[Math.floor(Math.random() * bulbs.length)];
+    return random;
+}
 
-//----function activeBulb(number) {
-    //const bulb = document.querySelector(`[data-lightbox='${number}']`);
+function switchLight(nextSerie) {
+    nextSerie.forEach((number, index) => {
+      setTimeout(() => {
+    
+      activateBulb(number);
+    }, (index + 1) * 600);
+  });
+}
 
-   // bulb.classList.remove('bulb-on');
+function activateBulb(number) {
+    const bulb = document.querySelector(`[data-key='${number}']`);
+    const sound = document.querySelector(`[data-sound='${number}']`);
+    
+    bulb.classList.remove('bulb-on');
 
-   // setTimeout(() => {
-       // bulb.classList.add('bulb-on');
-    //}, 400);
-//}
+    sound.play();    
+  
+    setTimeout(() => {
+        bulb.classList.add('hidden');
+    }, 400);
+}
 
-//function nextTap() {
-    //const 
-//}
+function userTurn(level) {
+  sectionContainer.classList.remove('unclickable');
+  sequenceLevel.textContent = `Your turn: ${level} Tap`;
+}
+
+
+function manageTap(bulb) {
+  const index = userSerie.push(bulb) - 1;
+  const remainingTaps = computerSerie.length - userSerie.length;
+  const sound = document.querySelector(`[data-sound='${bulb}']`);
+  sound.play();
+
+  if (userSerie[index] !== computerSerie[index]) {
+    gameLost.classList.remove('hidden');
+    resetGame(text);
+    return ;
+  }
+
+  if (userSerie.length === computerSerie.length) {
+    if (userSerie.length === 20) {
+      endGame.classList.remove('hidden')
+      resetGame(text);
+      return
+    }
+
+function resetGame() {
+  computerSerie = [];
+  userSerie = [];
+  level = 0;
+  startButton.classList.remove('hidden');
+  levelsCounter.classList.add('counter');
+  endGame.classList.remove('hidden')
+  sectionContainer.classList.add('unclickable');
+}
+
+    userserie = [];
+    sequencelevel.textContent = 'Well Done!';
+    setTimeout(() => {
+      nextRound();
+    }, 1000);
+    return;
+  }
+
+  sequenceLevel.textContent = `Your turn: ${remainingTaps} Tap${
+      remainingTaps 
+    }`;
+}
 
 
 
-   //const nextSerie = [computerSerie];
-   //nextSerie.push(nextTap());
-   //playLevel(nextSerie);
 
-   //computerSerie = [nextSerie];
-   //setTimeout(() => {
-   //userTurn(level);
-  //}, level * 500 + 1000);
-//}
-
-//function playLevel(nextSerie) {
-   // nextSequence.forEach((number, index) => {
-     // setTimeout(() => {
-        //  activeBulb(number);
-      //},(index + 1)* 500);
-   // });
-//}
